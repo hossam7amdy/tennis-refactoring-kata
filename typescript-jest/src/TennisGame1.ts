62fa3b44b7bb61320af387d8fcba8fd2f6631373
Hossam Hamdy <hossamhamdy117@gmail.com>
2025-12-24 22:13:09 +0200

    refactor(TennisGame1): Extract score formatting into getCurrentScore method
    - introduce Score enum for better readability.

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,4 +1,11 @@
-import { TennisGame } from './TennisGame';
+import { TennisGame } from "./TennisGame";
+
+enum Score {
+  "Love",
+  "Fifteen",
+  "Thirty",
+  "Forty",
+}
 
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
@@ -12,59 +19,42 @@ export class TennisGame1 implements TennisGame {
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
+    if (playerName === "player1") this.m_score1 += 1;
+    else this.m_score2 += 1;
   }
 
   getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
+    let score: string = "";
     if (this.m_score1 === this.m_score2) {
       switch (this.m_score1) {
         case 0:
-          score = 'Love-All';
+          score = "Love-All";
           break;
         case 1:
-          score = 'Fifteen-All';
+          score = "Fifteen-All";
           break;
         case 2:
-          score = 'Thirty-All';
+          score = "Thirty-All";
           break;
         default:
-          score = 'Deuce';
+          score = "Deuce";
           break;
-
       }
-    }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
       const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    }
-    else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
-        }
-      }
+      if (minusResult === 1) score = "Advantage player1";
+      else if (minusResult === -1) score = "Advantage player2";
+      else if (minusResult >= 2) score = "Win for player1";
+      else score = "Win for player2";
+    } else {
+      score = this.getCurrentScore();
     }
     return score;
+  }
+
+  private getCurrentScore(): string {
+    const player1Score = Score[this.m_score1];
+    const player2Score = Score[this.m_score2];
+    return `${player1Score}-${player2Score}`;
   }
 }
